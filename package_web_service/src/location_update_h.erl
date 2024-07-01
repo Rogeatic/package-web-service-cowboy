30d9a5fb1e0b51531041bad1200383550fd33119
Logan Bell <97977799+Rogeatic@users.noreply.github.com>
2024-07-01 10:36:14 -0600

    added stuff that works now

--- package_web_service/src/location_update_h.erl
+++ package_web_service/src/location_update_h.erl
@@ -1,1 +1,17 @@
 -module(location_update_h).
+
+-export([init/2]).
+
+init(Req0, Opts) ->
+    {ok, Data,_} = cowboy_req:body(Req0),
+
+    % FIX THIS LINE and include jsx
+    % {Location_id, Package_id}
+    DataRecieved = jsx:decode(Data),
+    io:format(DataRecieved),
+    %Result = case 
+
+    Req = cowboy_req:reply(200, #{
+           <<"content-type">> => <<"text/plain">>
+    }, "", Req0),
+    {ok, Req, Opts}.
