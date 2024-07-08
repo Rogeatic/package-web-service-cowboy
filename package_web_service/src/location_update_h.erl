e8959a7dce0ba4368cd7ede2fded2e2a870c649e
Logan Bell <97977799+Rogeatic@users.noreply.github.com>
2024-07-08 10:41:20 -0600

    added to location update

--- package_web_service/src/location_update_h.erl
+++ package_web_service/src/location_update_h.erl
@@ -3,15 +3,17 @@
 -export([init/2]).
 
 init(Req0, Opts) ->
-    {ok, Data,_} = cowboy_req:read_body(Req0),
+       {ok, Data,_} = cowboy_req:read_body(Req0),
 
-    % FIX THIS LINE and include jsx
-    % {Location_id, Package_id}
-    DataRecieved = jsx:decode(Data),
-    io:format("~s~n", [DataRecieved]),
-    %Result = case 
+       % FIX THIS LINE and include jsx
+       % {Location_id, Package_id}
+       DataRecieved = jsx:decode(Data),
+       io:format("~s~n", [jsx:encode(DataRecieved)]),
+       Indicator = case erpc:call("businesslogic.williamsonline.net", "package_server", "update_location", [
+       maps:get(location_id, DataRecieved),maps:get(long, DataRecieved),  maps:get(lat, DataRecieved)]) of 
+              worked -> 200;
+              _ -> 500
+       end,
+       Req = cowboy_req:reply(Indicator, #{ <<"content-type">> => <<"text/plain">>}),
 
-    Req = cowboy_req:reply(200, #{
-           <<"content-type">> => <<"text/plain">>
-    }, "", Req0),
-    {ok, Req, Opts}.
+       {ok, Req, Opts}.
