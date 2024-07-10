14c612a5da7f9c1f1d8f219c76a9515b642662f6
Logan Bell <97977799+Rogeatic@users.noreply.github.com>
2024-07-10 11:01:51 -0600

    fixed delivered

--- package_web_service/src/delivered_h.erl
+++ package_web_service/src/delivered_h.erl
@@ -3,10 +3,16 @@
 -export([init/2]).
 
 init(Req0, Opts) ->
-    {ok, Data,_} = cowboy_req:read_body(Req0),
+       {ok, Data,_} = cowboy_req:read_body(Req0),
+       DataRecieved = jsx:decode(Data),
+       PackageId = maps:get(<<"package_id">>, DataRecieved),
+       
+       io:format("Package ID: ~p~n", [PackageId]),
+       io:format("~s~n", [jsx:encode(DataRecieved)]),
+       Indicator = case erpc:call('bus@businesslogic.williamsonline.net', 'package_server', 'delivered', [PackageId]) of 
+              worked -> 200;
+              _ -> 500
+              end,
+       Req = cowboy_req:reply(Indicator, #{ <<"content-type">> => <<"text/plain">>}, Req0),
 
-    DataRecieved = jsx:decode(Data),
-    io:format("~s~n", [DataRecieved]),
-
-    Req = cowboy_req:reply(200, #{ <<"content-type">> => <<"text/plain">>}, "", Req0),
-    {ok, Req, Opts}.
+       {ok, Req, Opts}.
