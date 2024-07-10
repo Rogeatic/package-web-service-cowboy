fa42f12c17e4a109817f9cc4948e51192e436572
Logan Bell <97977799+Rogeatic@users.noreply.github.com>
2024-07-10 10:41:47 -0600

    debug print

--- package_web_service/src/delivered_h.erl
+++ package_web_service/src/delivered_h.erl
@@ -5,13 +5,8 @@
 init(Req0, Opts) ->
     {ok, Data,_} = cowboy_req:read_body(Req0),
 
-    % FIX THIS LINE and include jsx
-    % {Location_id, Package_id}
     DataRecieved = jsx:decode(Data),
     io:format("~s~n", [DataRecieved]),
-
-    
-    %Result = case 
 
     Req = cowboy_req:reply(200, #{ <<"content-type">> => <<"text/plain">>}, "", Req0),
     {ok, Req, Opts}.
--- package_web_service/src/location_request_h.erl
+++ package_web_service/src/location_request_h.erl
@@ -4,10 +4,10 @@
 
 init(Req0, Opts) ->
        {ok, Data,_} = cowboy_req:read_body(Req0),
-       Location_id_recieved = jsx:decode(Data),
+       Package_id_recieved = jsx:decode(Data),
        
-       io:format("Location ID: ~p~n", [Location_id_recieved]),
-       Req = case erpc:call('bus@businesslogic.williamsonline.net', 'package_server', 'location_request', [Location_id_recieved]) of 
+       io:format("Location ID: ~p~n", [Package_id_recieved]),
+       Req = case erpc:call('bus@businesslogic.williamsonline.net', 'package_server', 'location_request', [Package_id_recieved]) of 
               {worked, Long, Lat} ->
                      cowboy_req:reply(200, #{ <<"content-type">> => <<"text/plain">>}, jsx:encode({Long, Lat}), Req0);
               _ ->
