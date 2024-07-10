986c580905b7eb21f30bd089e9d627d64091da87
Logan Bell <97977799+Rogeatic@users.noreply.github.com>
2024-07-10 10:16:42 -0600

    fixed location request

--- package_web_service/src/location_request_h.erl
+++ package_web_service/src/location_request_h.erl
@@ -3,13 +3,15 @@
 -export([init/2]).
 
 init(Req0, Opts) ->
-    {ok, Data,_} = cowboy_req:read_body(Req0),
-
-    % FIX THIS LINE and include jsx
-    % {Location_id, Package_id}
-    DataRecieved = jsx:decode(Data),
-    io:format("~s~n", [DataRecieved]),
-    %Result = case 
-
-    Req = cowboy_req:reply(200, #{ <<"content-type">> => <<"text/plain">> }, "", Req0),
-    {ok, Req, Opts}.
+       {ok, Data,_} = cowboy_req:read_body(Req0),
+       Location_id_recieved = jsx:decode(Data),
+       
+       io:format("Location ID: ~p~n", [Location_id_recieved]),
+       Req = case erpc:call('bus@businesslogic.williamsonline.net', 'package_server', 'location_request', [Location_id_recieved]) of 
+              {worked, Long, Lat} ->
+                     cowboy_req:reply(200, #{ <<"content-type">> => <<"text/plain">>}, jsx:encode({Long, Lat}), Req0);
+              _ ->
+                     cowboy_req:reply(500, #{ <<"content-type">> => <<"text/plain">>}, Req0)
+              end,
+       % returns {worked, Long, Lat}
+       {ok, Req, Opts}.
