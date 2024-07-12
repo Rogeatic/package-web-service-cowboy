5ee1f0ad70528d18b0cc92966e0614aaae3b913f
Logan Bell <97977799+Rogeatic@users.noreply.github.com>
2024-07-12 10:57:51 -0600

    changed to decode

--- package_web_service/src/location_request_h.erl
+++ package_web_service/src/location_request_h.erl
@@ -4,10 +4,11 @@
 
 init(Req0, Opts) ->
        {ok, Data,_} = cowboy_req:read_body(Req0),
-       Package_id_recieved = jsx:decode(Data),
-       
-       io:format("Location ID: ~p~n", [Package_id_recieved]),
-       Req = case erpc:call('bus@businesslogic.williamsonline.net', 'package_server', 'location_request', [Package_id_recieved]) of 
+       DataRecieved = jsx:decode(Data),
+       PackageId = maps:get(<<"package_id">>, DataRecieved),
+
+       io:format("Location ID: ~p~n", [PackageId]),
+       Req = case erpc:call('bus@businesslogic.williamsonline.net', 'package_server', 'location_request', [PackageId]) of 
               {worked, Long, Lat} ->
                      cowboy_req:reply(200, #{ <<"content-type">> => <<"text/plain">>}, jsx:encode({Long, Lat}), Req0);
               _ ->
