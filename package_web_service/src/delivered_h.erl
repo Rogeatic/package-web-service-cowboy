8b34fb55657311a9d973f92a48c1c07ff38825af
Logan Bell <97977799+Rogeatic@users.noreply.github.com>
2024-07-15 10:56:23 -0600

    remove comments

--- package_web_service/src/delivered_h.erl
+++ package_web_service/src/delivered_h.erl
@@ -5,12 +5,6 @@
 init(Req0, Opts) ->
        {ok, Data,_} = cowboy_req:read_body(Req0),
        Package_id = jsx:decode(Data),
-       
-       io:format("Package ID: ~p~n", [Data]),
-       io:format("~s~n", [jsx:encode(Package_id)]),
-       io:format("~s~n", [Package_id]),
-       io:format("~s~n", ["Hello World"]),
-
 
        Indicator = case erpc:call('bus@businesslogic.williamsonline.net', 'package_server', 'delivered', [Package_id]) of 
               worked -> 200;
