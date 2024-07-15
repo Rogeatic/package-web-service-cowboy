4cba8b43fa72ca1ebb40975dcebdf47fc59ed2a5
Logan Bell <97977799+Rogeatic@users.noreply.github.com>
2024-07-15 10:49:37 -0600

    remove debug

--- package_web_service/src/location_request_h.erl
+++ package_web_service/src/location_request_h.erl
@@ -6,7 +6,6 @@ init(Req0, Opts) ->
        {ok, Data,_} = cowboy_req:read_body(Req0),
        Package_id = jsx:decode(Data),
 
-       io:format("ERP result: ~p~n", [erpc:call('bus@businesslogic.williamsonline.net', 'package_server', 'location_request', [Package_id])]),
        Req = case erpc:call('bus@businesslogic.williamsonline.net', 'package_server', 'location_request', [Package_id]) of 
               {worked, Long, Lat} ->
                      cowboy_req:reply(200, #{ <<"content-type">> => <<"text/plain">>}, jsx:encode(#{<<"long">> => Long, <<"lat">> => Lat}), Req0);
